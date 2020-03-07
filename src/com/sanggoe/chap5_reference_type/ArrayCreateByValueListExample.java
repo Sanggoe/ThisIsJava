e7c4df3cbbb1efa4272daa3aa98b3d28b9f8b303
SANG-MIN PARK <sanggoe0509@gmail.com>
2020-03-08 00:13:24 +0900

    chap5 시작

---
+++ src/com/sanggoe/chap5_reference_type/ArrayCreateByValueListExample.java
@@ -0,0 +1,26 @@
+package com.sanggoe.chap5_reference_type;
+
+public class ArrayCreateByValueListExample {
+    public static void main(String[] args) {
+        int[] scores;
+        scores = new int[]{83, 90, 87};
+        int sum1 = 0;
+        for (int i : scores) {
+            sum1 += i;
+        }
+        System.out.println("Total : " + sum1);
+
+        int sum2 = add(new int[]{83, 90, 87});
+        System.out.println("Total : " + sum2);
+        System.out.println();
+    }
+
+    public static int add(int[] scores) {
+        int sum = 0;
+        for (int i : scores) {
+            sum += i;
+        }
+        return sum;
+    }
+
+}
