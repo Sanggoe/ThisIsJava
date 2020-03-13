cc26a1886d62ab454730dd02cf38f6416db569a4
SANG-MIN PARK <sanggoe0509@gmail.com>
2020-03-13 13:56:58 +0900

    chap9 복습 시작

---
+++ src/com/sanggoe/chap9_nested_class_interface/access_modifier/A.java
@@ -0,0 +1,4 @@
+package com.sanggoe.chap9_nested_class_interface.access_modifier;
+
+public class A {
+}
---
+++ src/com/sanggoe/chap9_nested_class_interface/nested_class/Main.java
@@ -0,0 +1,22 @@
+package com.sanggoe.chap9_nested_class_interface.nested_class;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+
+        // 인스턴트 멤버 클래스 객체 생성
+        A.B b = a.new B();
+        b.field1 = 3;
+        b.method1();
+
+        // 정적 멤버 클래스 객체 생성
+        A.C c = new A.C();
+        c.field1 = 3;
+        c.method1();
+        A.C.field2 = 3;
+        A.C.mathod2();
+
+        // 로컬 클래스 객체 생성을 위한 메소드 호출
+        a.method();
+    }
+}
